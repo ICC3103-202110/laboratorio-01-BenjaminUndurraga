87c4e9ac514dd7063ad1d33b74f3aa2deba43e1c
BenjaminUndurraga <80711801+BenjaminUndurraga@users.noreply.github.com>
2021-03-26 18:36:17 -0300

    Add files via upload

--- laboratorio1.py
+++ laboratorio1.py
@@ -24,6 +24,7 @@ while len(cards)>0:
     messing_up = cards[random.randint(len(cards))]
     messy_cards.append(messing_up)
     cards.remove(messing_up)
+
 '''
 sacamos las cartas de cards y las pusimos en messy_cards desordenadas.
 ahora tenemos que pasar messy_cards a matriz para que sea un tablero con coordenadas.
@@ -44,27 +45,38 @@ def silenced_board(rows,amount):
         m.append(rows)
     return m
 
-'''
-def move(x1,y1): 
+coords1=[]
+coords2=[]
+while total_points < cards_to_play:
+    print(silenced_board(2,cards_to_play))
+    coordx = int(input('coord x: '))
+    coordy = int(input('coord y: '))
+    j1=board[coordx-1][coordy-1]
+    coords1.append(coordx)
+    coords1.append(coordy)
+    print(j1)
 
-print(move)
-
-me falta remover, imprimir tablero
-'''
-print(silenced_board(2,cards_to_play))
-coordx = int(input('coord x: '))
-coordy = int(input('coord y: '))
-print(board[coordx-1][coordy-1])
-
-coordx2 = int(input('coord x: '))
-coordy2 = int(input('coord y: '))
-print(board[coordx2 - 1][coordy2 - 1])
-while coordx <=2:
-    if board[coordx-1][coordy-1] == board[coordx2-1][coordy2-1]:
-        print('correct! u can continue')
-        board.remove([coordx-1][coordy-1])
-        board.remove([coordx2-1][coordy2-1])
+    coordx2 = int(input('coord x: '))
+    coordy2 = int(input('coord y: '))
+    j2=board[coordx2 - 1][coordy2 - 1]
+    coords2.append(coordx2)
+    coords2.append(coordy2)
+    print(j2)
+    
+    if coordx <=2 and coordy <= cards_to_play and coords1 != coords2:
+        if board[coordx-1][coordy-1] == board[coordx2-1][coordy2-1]:
+            print('correct! u can continue')
+            np.delete(board,j1)
+            np.delete(board,j2)
+            np.delete(silenced_board,j1)
+            np.delete(silenced_board,j2)
+            total_points+=1
+        else:
+            print('incorrect :( turn of player 2. ')
     else:
-        print('incorrect :( turn of player 2. ')
-        
-print(silenced_board(2,cards_to_play))
+        print('invalid coordenates')
+    print(silenced_board(2,cards_to_play))
+    turn+=1
+'''
+tengo que estudiar progra :( , perdon por el desorden, hice varios borradores
+'''
